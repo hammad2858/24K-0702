f5a46ccbc0e69711399b60be54f577671af77283
hammad2858 <muhammadhammadcr7@gmail.com>
2024-10-21 08:58:26 +0500

    Add files via upload

---
+++ PF LAB/Lab 8/problem1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+	int i,j;
+	for (i=1;i<=4;i++){
+		for (j=1;j<=i;j++){
+			printf("%d",j);
+			
+			
+			
+		}//for j ends
+		printf("\n");
+	}// for i ends
+	
+	
+	
+	
+	
+	return 0;
+}

6e6fb6e5f9bd828a5d0d5f7f4f1274c436f1257b
hammad2858 <muhammadhammadcr7@gmail.com>
2024-10-26 17:06:54 +0500

    Update problem1.c

--- PF LAB/Lab 8/problem1.c
+++ PF LAB/Lab 8/problem1.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-int main (){
-	int i,j;
-	for (i=1;i<=4;i++){
-		for (j=1;j<=i;j++){
-			printf("%d",j);
-			
-			
-			
-		}//for j ends
-		printf("\n");
-	}// for i ends
-	
-	
-	
-	
-	
-	return 0;
+int main(){
+    int i,j,num;
+    printf("Enter number :");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++){
+        for (j=1;j<=i;j++){
+            printf("%d ",j);
+        }// for j ends
+        printf("\n");
+    }// for i ends
+    
+    
+    return 0;
 }

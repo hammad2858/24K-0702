97d34ebedb8b990aeb8434a3ae2620acd99a0982
hammad2858 <muhammadhammadcr7@gmail.com>
2024-11-09 20:16:12 +0500

    Update q3.c

--- PF LAB/LAB 9/q3.c
+++ PF LAB/LAB 9/q3.c
@@ -2,24 +2,24 @@
 #include <string.h>
 
 int main() {
-    char destination[100], source[100];
+    char destination[100], str[100];
     int n;
 
     printf("Enter the destination string: ");
     scanf("%s", destination);
 
     printf("Enter the source string: ");
-    scanf("%s", source);
+    scanf("%s", str);
 
     printf("Enter the number of characters to append: ");
     scanf("%d", &n);
 
-    if (n > strlen(source)) {
-        n = strlen(source);
+    if (n > strlen(str)) {
+        n = strlen(str);
     }
 
     // Concatenate the first n characters of the source string to the destination
-    strncat(destination , source, n);
+    strncat(destination , str, n);
 
     printf("The new concatenated string is: %s\n", destination);
 
